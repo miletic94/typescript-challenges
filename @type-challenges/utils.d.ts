6b0f9fd40f310384868068abbbada8486a588fcf
miletic94 <mileticnemanja.lab@gmail.com>
2024-08-22 14:05:23 +0200

    Readonly, DeepReadonly

--- @type-challenges/utils.d.ts
+++ @type-challenges/utils.d.ts
@@ -6,3 +6,13 @@ export type Equal<A, B> = (<T>() => T extends A ? 1 : 2) extends <
   : false;
 
 export type Expect<T extends true> = T;
+
+type Alike<X, Y> = (<T>() => T extends MergeInsertions<X> ? 1 : 2) extends <
+  T
+>() => T extends MergeInsertions<Y> ? 1 : 2
+  ? true
+  : false;
+
+export type MergeInsertions<T> = T extends object
+  ? { [P in typeof T]: T[P] }
+  : T;
