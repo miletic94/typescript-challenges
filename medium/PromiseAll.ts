4383a21ee3603f310a93d69fa2c9a017b0ce8056
miletic94 <mileticnemanja.lab@gmail.com>
2024-08-23 16:06:36 +0200

    LookupType, PromiseAll

---
+++ medium/PromiseAll.ts
@@ -0,0 +1,61 @@
+/*
+  20 - Promise.all
+  -------
+  by Anthony Fu (@antfu) #medium #array #promise
+
+  ### Question
+
+  Type the function `PromiseAll` that accepts an array of PromiseLike objects, the returning value should be `Promise<T>` where `T` is the resolved result array.
+
+  ```ts
+  const promise1 = Promise.resolve(3);
+  const promise2 = 42;
+  const promise3 = new Promise<string>((resolve, reject) => {
+    setTimeout(resolve, 100, 'foo');
+  });
+
+  // expected to be `Promise<[number, 42, string]>`
+  const p = PromiseAll([promise1, promise2, promise3] as const)
+  ```
+
+  > View on GitHub: https://tsch.js.org/20
+*/
+
+/* _____________ Your Code Here _____________ */
+type PromiseAll<T extends any[]> = Promise<{
+  [K in keyof T]: Awaited<T[K]>;
+}>;
+
+declare function PromiseAll<T extends any[]>(values: [...T]): PromiseAll<T>;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+const promiseAllTest1 = PromiseAll([1, 2, 3] as const);
+const promiseAllTest2 = PromiseAll([1, 2, Promise.resolve(3)] as const);
+const promiseAllTest3 = PromiseAll([1, 2, Promise.resolve(3)]);
+const promiseAllTest4 = PromiseAll<Array<number | Promise<number>>>([1, 2, 3]);
+
+type cases = [
+  Expect<Equal<typeof promiseAllTest1, Promise<[1, 2, 3]>>>,
+  Expect<Equal<typeof promiseAllTest2, Promise<[1, 2, number]>>>,
+  Expect<Equal<typeof promiseAllTest3, Promise<[number, number, number]>>>,
+  Expect<Equal<typeof promiseAllTest4, Promise<number[]>>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/20/answer
+  > View solutions: https://tsch.js.org/20/solutions
+  > More Challenges: https://tsch.js.org
+*/
+const arr = [1, 2, 3];
+type Arr<T extends readonly any[]> = {
+  [K in keyof T]: T[K];
+};
+function processTuple<T extends readonly any[]>(tuple: T) {
+  type A = Arr<T>;
+  // A will preserve the tuple structure
+}
+
+const a = processTuple([1, 2, 3]); // A is readonly [1, 2, 3]
